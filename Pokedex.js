9f2f17d2a67e6364d5d7ae72bfba17c75e57fda7
geraldinebil <113667275+geraldinebil@users.noreply.github.com>
2023-05-19 15:12:32 +0200

    yvyuvfy

--- Pokedex.js
+++ Pokedex.js
@@ -1,5 +1,15 @@
 const pokedex = document.getElementById('pokedex');
 
+var container = document.body.querySelector("#app");
+var pokemonToFetch = 151;
+var searchBar = document.body.querySelector("#search-bar");
+var searchBtn = document.body.querySelector("#submit-btn");
+var anchorSearch = document.body.querySelector("#searchClick");
+var newPokeImg = function (pokeID) {
+    var src = "https://pokeres.bastionbot.org/images/pokemon/" + pokeID + ".png";
+    return src;
+};
+
 const fetchPokemon = () => {
     const promises = [];
     for (let i = 1; i <= 150; i++) {
@@ -34,3 +44,37 @@ const displayPokemon = (pokemon) => {
 };
 
 fetchPokemon();
+
+var mySearchStuff = function () {
+    var inputStr = "";
+    searchBar.addEventListener("keyup", function (e) {
+        e.preventDefault();
+        inputStr = e.target.value;
+    });
+}
+var promiseArr = [];
+    for (var i = 1; i <= pokemonToFetch; i++) {
+        var url = "https://pokeapi.co/api/v2/pokemon/" + i;
+        promiseArr.push(fetch(url).then(function (data) { return data.json(); }));
+    }
+    Promise.all(promiseArr).then(function (data) {
+        var pokemon = data
+            .map(function (poke) { return ({
+            name: poke.name.slice(0, 1).toUpperCase().concat(poke.name.slice(1).toLowerCase()),
+            id: poke.id,
+            height: poke.height,
+            weight: poke.weight
+        }); });
+        searchBtn.addEventListener("click", function () {
+            var names = pokemon.map(function (val) { return val.name; });
+            for (var i = 0; i <= pokemonToFetch; i++) {
+                if (inputStr === names[i]) {
+                    anchorSearch.href = "#" + names[i];
+                }
+            }
+            if (inputStr === "") {
+                anchorSearch.href = "#";
+            }
+        });
+    });
+mySearchStuff();
