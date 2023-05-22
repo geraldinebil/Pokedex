7894ac5eab395a30012f1123c8c1c15759312937
geraldinebil <113667275+geraldinebil@users.noreply.github.com>
2023-05-22 15:26:37 +0200

    barre de recherche en cours

--- Pokedex.js
+++ Pokedex.js
@@ -1,41 +1,28 @@
 const pokedex = document.getElementById('pokedex');
 
-var container = document.body.querySelector("#app");
-var pokemonToFetch = 151;
-var searchBar = document.body.querySelector("#search-bar");
-var searchBtn = document.body.querySelector("#submit-btn");
-var anchorSearch = document.body.querySelector("#searchClick");
-var newPokeImg = function (pokeID) {
-    var src = "https://pokeres.bastionbot.org/images/pokemon/" + pokeID + ".png";
-    return src;
-};
 
-const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 150; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
-    }
-    Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
-        }));
-        displayPokemon(pokemon);
-    });
-};
+const fetchPokemon = async () => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const pokemon = data.results.map((result, index) => ({
+      ...result,
+      id: index + 1,
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +
+        1}.png`,
+    }));
+    displayPokemon(pokemon);
+  };
 
 const displayPokemon = (pokemon) => {
-    console.log(pokemon);
+    
     const pokemonHTMLString = pokemon
         .map(
             (pokeman) => `
-        <li class="card">
+        <li class="card" onclick = "selectPokemon(${pokeman.id})">
             <img class="card-image" src="${pokeman.image}"/>
             <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-            <p class="card-subtitle">Type: ${pokeman.type}</p>
+            
         </li>
     `
         )
@@ -43,38 +30,46 @@ const displayPokemon = (pokemon) => {
     pokedex.innerHTML = pokemonHTMLString;
 };
 
+const selectPokemon = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokeman = await res.json();
+    displayPokemanPopup(pokeman);
+
+    
+  };
+
+  const displayPokemanPopup = pokeman => {
+    const type = pokeman.types.map((type) => type.type.name).join(", ");
+    const image = pokeman.sprites ['front_default'];
+    const htmlString = ` 
+    <div class="popup"> 
+    <button id="closeBtn" onclick="closePopup()">Close</button> 
+    <div class="card"> 
+    <img class="card-image" src="${image}"/> 
+    <h2 class="card-title">${pokeman.id} </h2> 
+    <small>Height:</small> ${pokeman.height} | 
+    <small>Weight:</small> ${pokeman.weight}|
+    <small>Type:</small> ${type}
+    </div> 
+    </div> `
+    ;
+    pokedex.innerHTML = htmlString + pokedex.innerHTML;
+    console.log (htmlString);
+  };
+
+  const closePopup = () => {
+    const popup = document.querySelector ('.popup');
+    popup.parentElement.removeChild (popup);
+  }
+
 fetchPokemon();
 
-var mySearchStuff = function () {
-    var inputStr = "";
-    searchBar.addEventListener("keyup", function (e) {
-        e.preventDefault();
-        inputStr = e.target.value;
-    });
-}
-var promiseArr = [];
-    for (var i = 1; i <= pokemonToFetch; i++) {
-        var url = "https://pokeapi.co/api/v2/pokemon/" + i;
-        promiseArr.push(fetch(url).then(function (data) { return data.json(); }));
-    }
-    Promise.all(promiseArr).then(function (data) {
-        var pokemon = data
-            .map(function (poke) { return ({
-            name: poke.name.slice(0, 1).toUpperCase().concat(poke.name.slice(1).toLowerCase()),
-            id: poke.id,
-            height: poke.height,
-            weight: poke.weight
-        }); });
-        searchBtn.addEventListener("click", function () {
-            var names = pokemon.map(function (val) { return val.name; });
-            for (var i = 0; i <= pokemonToFetch; i++) {
-                if (inputStr === names[i]) {
-                    anchorSearch.href = "#" + names[i];
-                }
-            }
-            if (inputStr === "") {
-                anchorSearch.href = "#";
-            }
-        });
-    });
-mySearchStuff();
+searchInput.addEventListener ('input', function (e) {
+    const pokeman = document.querySelector(".pokeman"). toLowerCase ();
+    
+    const search = searchInput.value.toLowerCase();
+    
+});
+
+
